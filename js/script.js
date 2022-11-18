c95fa6fe3ecf57c8e4150b3291253956a3b25e1b
Mark Taratynov <mark.taratynov@gmail.com>
2022-11-18 23:59:12 +0100

    try spa

--- js/script.js
+++ js/script.js
@@ -1,48 +1,76 @@
-let requestObj = new XMLHttpRequest();
+     //находим коробку под карточки
+    let content = document.getElementById('content');
 
-    requestObj.open('GET','https://kitsu.io/api/edge/anime', false);
+    //получаем текст шаблона каталога
+    let templateCatalog = document.getElementById('tmpl-catalog').innerHTML;
 
-    requestObj.send();
+    //получаем текст шаблона карточки
+    let templateCard = document.getElementById('tmpl-card').innerHTML;
 
-    let array = JSON.parse(requestObj.responseText);
+    //вызываем функцию при загрузке страницы
+    renderCatalog();
 
-    console.log(array);
+    //функция отрисовки каталога
+    function renderCatalog() {
 
-    for (let i = 0; i < array['data'].length; i++ ) {
+        //очищаем страницу
+        clearPage();
 
-    document.getElementById('pic').innerHTML += ` 
-        <div class="name" style="display: flex; justify-content: space-around; align-items: center;border: 1px solid black; border-radius: 20px; box-shadow: 3px 5px 10px black; width: 250px; height: 450px; border-size: border-box; flex-direction: column; color: white; margin: 15px 15px; font-family: 'Josefin Sans', sans-serif;">
-            ${array['data'][i]['attributes']['titles']['en_jp']}
-        <div>
-        <img src='${array['data'][i]['attributes']['posterImage']['large']}' 
-        style=";
-        margin-bottom: 20px;
-        width: 200px;
-        height: 330px;
-        min-width: 200px;
-        object-fit: fill;
-        border-radius: 10px;
-        box-shadow: 3px 5px 10px black;
-        flex-shrink: 0;">
-        <div style="color: black; width: 60px; height: 25px; line-height: 25px; background-color: yellow; text-align: center; border: 1px solid black; box-sizing: border-box; border-radius: 10px; box-shadow: 2px 5px 10px black;">R: 
-            ${array['data'][i]['attributes']['averageRating']}
-        </div>
-        <div style="display: flex;
-        justify-content: end;
-        font-size: 15px;
-        font-family: 'Josefin Sans', sans-serif;
-        width: 200px;
-        height: 25px;
-        text-align: center;
-        line-height: 25px;
-        margin-top: 10px;">
-            Count Series:  
-            ${array['data'][i]['attributes']['episodeCount']}
-        </div>
-        `;
-}
+        //получаем данные каталога
+        let json = sendRequestGet("https://kitsu.io/api/edge/anime" );
 
-    document.getElementById('footer').innerHTML += `
-        <div class="bottom" style="width: 100%; height: 100px; background-color: rgba(104, 219, 253, 0.156); border-top: 1px solid black; display: flex; justify-content: center; align-items: center; font-size: 20px; font-weight: bolder; font-family: 'Josefin Sans', sans-serif;">
-        <div>Copyright 2022 | <em style="color: darkblue;">Anime Cards</em> Mark Taratynov</div>
-    `;
+        //раскодируем данные
+        let data = JSON.parse(json);
+
+        //рисуем данные на экран
+        for (let i = 0; i < data['data'].length; i++ ) {
+
+            //заполняем шаблон и выводим на экран
+            content.innerHTML += templateCatalog.replace('${title}',data['data'][i]['attributes']['titles']['en_jp'])
+                                                .replace('${id}',data['data'][i]['id'])
+                                                .replace('${photo}',data['data'][i]['attributes']['posterImage']['large'])
+                                                .replace('${rating}',data['data'][i]['attributes']['averageRating'])
+                                                .replace('${count}', data['data'][i]['attributes']['episodeCount']) ;
+        }
+    }
+
+    //Функция отрисовки карточки
+    function renderCard(id) {
+
+        //Очищаем страницу
+        clearPage();
+
+        //Получаем данные каталога
+        let json = sendRequestGet('https://kitsu.io/api/edge/anime/' + id);
+
+        //Раскодируем данные
+        let data = JSON.parse(json);
+
+        content.innerHTML += templateCard.replace('${title}', data['data']['attributes']['titles']['en_jp'])
+                                .replace('${photo}', data['data']['attributes']['posterImage']['large'])
+                                .replace('${rating}', data['data']['attributes']['averageRating'])
+                                .replace('${count}', data['data']['attributes']['episodeCount'])
+                                .replace('${year}', data['data']['attributes']['startDate'])
+                                .replace('${end}', data['data']['attributes']['endDate'])
+                                .replace('${ratingRank}', data['data']['attributes']['ratingRank'])
+                                .replace('${views}', data['data']['attributes']['userCount'])
+                                .replace('${description}', data['data']['attributes']['description'])
+                                ;
+        }
+    
+
+
+    //функция очистки страницы
+    function clearPage(){
+        document.getElementById('content').innerHTML = '';
+    }
+
+    //функция для отправки запросов
+    function sendRequestGet(url) {
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.send()
+
+        return xhr.responseText;
+    }
